2c6e89b5971d26e829fd769adb79873c99143494
Kyle Wacker <144162723+kwacke@users.noreply.github.com>
2024-02-21 12:48:04 -0500

    Add files via upload

---
+++ src/components/UserGame.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ correctWord }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      correctWord:'',
+    }
+  },
+  created(){
+Object.keys(this.correctWord) = this.$store.commit("GET_RANDOM_WORD")
+Object.value(this.correctWord) = this.$store.commit("GET_RANDOM_WORD")
+
+  }
+}
+</script>
+<style></style>
