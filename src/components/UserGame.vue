e913ed88db1d2b9aeaaf1d0f5ad7c89bb0297856
Kyle Wacker <kylewwacker@gmail.com>
2024-03-02 09:35:09 -0500

    updating methods inside of userGame

--- src/components/UserGame.vue
+++ src/components/UserGame.vue
@@ -9,7 +9,7 @@
         <br><br>
         <p class="hint">Hint: {{ hint }}</p>
       </div>
-      <user-guess/>
+      <user-guess />
     </div>
   </div>
 </template>
@@ -18,13 +18,17 @@
 import UserGuess from '../components/UserGuess.vue';
 
 export default {
-  components:{UserGuess
+  components: {
+    UserGuess
   },
   data() {
     return {
       correctWord: '',
       hint: '',
       img: './YinzerHangman.jpg',
+      hidden: '',
+      incorrect: 0,
+      guess: ''
     };
   },
   created() {
@@ -38,9 +42,44 @@ export default {
     hideWord() {
       // Mask the characters of the currentWord with underscores
       this.hidden = this.currentWord.replace(/\w/g, '_ ').trim();
+    },
+    submitGuess() {
+      if (this.guess.trim().toLowerCase() === '') {
+        alert('Please enter a guess!');
+        return;
+      }
+    },
+    displayGameOutcome() {
+      if (this.hidden.includes('_') && this.incorrect == 5 || this.hidden != this.user.answer) {
+        console.log('Game over, ya Jag!')
+      }
+    },
+    askToPlayAgain() {
+      const response = prompt("Yinz wanna play again?\nJus' type in Y or Yes if yinz wanna play again, 'n if yinz had enough, type N or No to exit, y'know");
+      if (response && (response.toLowerCase() == 'n') || response.toLowerCase() == 'no') {
+        console.log("See Yinz again next time!")
+      }
+      else if (response && (response.toLowerCase() == 'y') || response.toLowerCase() == 'yes') {
+        //handle if yes
+      } else {
+        console.log("I'm sorry 'n'at, but could yinz please enter a Y to play again or an N to exit, jagoff")
+        this.askToPlayAgain();
+      }
+    },
+    handleInvalidPlayAgainInput() {
+      console.log("I'm sorry 'n'at, but could yinz please enter a Y to play again or an N to exit, jagoff")
+      this.askToPlayAgain();
+    },
+    resetGameVariables() {
+      this.user.correct = 0;
+      this.user.incorrect = 0;
+      this.user.hasLetter = false;
+      this.user.answer = "";
+      this.user.userGuess = "";
     }
   }
 };
+
 </script>
 <style>
 .container {
