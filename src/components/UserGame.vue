7c9a36744eca07f4d753854284a430f0ee6266d7
Kyle Wacker <kylewwacker@gmail.com>
2024-03-04 16:24:55 -0500

    more logic updates

--- src/components/UserGame.vue
+++ src/components/UserGame.vue
@@ -9,7 +9,7 @@
         <br><br>
         <p class="hint">Hint: {{ hint }}</p>
       </div>
-      <user-guess />
+      <user-guess @submit-guess="handleGuess" :answer="correctWord" :hidden="hidden" :maxIncorrect="maxIncorrect"/>
     </div>
   </div>
 </template>
@@ -28,7 +28,7 @@ export default {
       img: './YinzerHangman.jpg',
       hidden: '',
       incorrect: 0,
-      guess: ''
+      maxIncorrect: 5, //Max number of incorrect guesses
     };
   },
   created() {
@@ -41,45 +41,53 @@ export default {
   methods: {
     hideWord() {
       // Mask the characters of the currentWord with underscores
-      this.hidden = this.currentWord.replace(/\w/g, '_ ').trim();
+      this.hidden = this.currentWord.replace(/\w/g, '_').trim();
     },
-    submitGuess() {
-      if (this.guess.trim().toLowerCase() === '') {
-        alert('Please enter a guess!');
-        return;
-      }
+    handleGuess() {
+      // This method will be called by the UserGuess component when a guess is submitted.
     },
     displayGameOutcome() {
-      if (this.hidden.includes('_') && this.incorrect == 5 || this.hidden != this.user.answer) {
-        console.log('Game over, ya Jag!')
+      if (this.hidden.includes('_') && this.incorrect === this.maxIncorrect) {
+        console.log('Game over, ya Jag!');
+        this.askToPlayAgain();
+      } else if (!this.hidden.includes('_')) {
+        console.log('Yinz win!');
+        this.askToPlayAgain();
       }
     },
     askToPlayAgain() {
       const response = prompt("Yinz wanna play again?\nJus' type in Y or Yes if yinz wanna play again, 'n if yinz had enough, type N or No to exit, y'know");
-      if (response && (response.toLowerCase() == 'n') || response.toLowerCase() == 'no') {
-        console.log("See Yinz again next time!")
-      }
-      else if (response && (response.toLowerCase() == 'y') || response.toLowerCase() == 'yes') {
-        //handle if yes
+      if (response && (response.toLowerCase() === 'n' || response.toLowerCase() === 'no')) {
+        console.log("See Yinz again next time!");
+      } else if (response && (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes')) {
+        this.resetGameVariables();
       } else {
-        console.log("I'm sorry 'n'at, but could yinz please enter a Y to play again or an N to exit, jagoff")
-        this.askToPlayAgain();
+        this.handleInvalidPlayAgainInput();
       }
     },
     handleInvalidPlayAgainInput() {
-      console.log("I'm sorry 'n'at, but could yinz please enter a Y to play again or an N to exit, jagoff")
+      console.log("I'm sorry 'n'at, but could yinz please enter a Y to play again or an N to exit, jagoff");
       this.askToPlayAgain();
     },
     resetGameVariables() {
-      this.user.correct = 0;
-      this.user.incorrect = 0;
-      this.user.hasLetter = false;
-      this.user.answer = "";
-      this.user.userGuess = "";
+      this.correctWord = '';
+      this.hint = '';
+      this.incorrect = 0;
+      this.$store.commit("GET_RANDOM_WORD");
+      this.currentWord = this.$store.state.word;
+      this.hint = this.$store.state.hint;
+      this.hideWord();
+    },
+    updateHidden(newHidden){
+      this.hidden = newHidden;
+    },
+    gameWon(){
+      console.log('Congratulations! You won the game!')
+      this.resetGameVariables();
+      this.askToPlayAgain();
     }
   }
 };
-
 </script>
 <style>
 .container {
--- src/components/UserGuess.vue
+++ src/components/UserGuess.vue
@@ -1,44 +1,101 @@
 <template>
-    <div class="user-guess">
-      <form @submit.prevent="submitGuess">
-        <input type="text" v-model="guess" placeholder="Enter a guess, ya jag">
-        <button class="button" type="submit">Enter Your Guess!</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        guess: '',
-        incorrect: '',
-      };
-    },
-    methods: {
-      submitGuess() {
-        if (this.guess.trim().toLower() === '') {
-          alert('Please enter a guess!');
+  <div class="user-guess">
+    <form @submit.prevent="submitGuess">
+      <input type="text" v-model="guess" placeholder="Enter a guess, ya jag">
+      <button class="button" type="submit">Enter Your Guess!</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['answer', 'hidden', 'maxIncorrect'],
+  data() {
+    return {
+      guess: '',
+      incorrect: 0,
+    };
+  },
+  methods: {
+    submitGuess() {
+      if (this.guess.trim().toLowerCase() === '') {
+        alert('Please enter a guess!');
+      } else {
+        if (this.isSingleLetterGuess(this.guess)) {
+          this.handleSingleLetterGuess();
         } else {
-          console.log('User guessed:', this.guess);
-          this.guess = '';
+          if (this.isFullwordGuess(this.guess, this.answer)) {
+            this.handleFullWordGuess();
+          }
+        }
+        this.guess = '';
+      }
+    },
+    isSingleLetterGuess(guess) {
+      return guess.length === 1;
+    },
+    isFullwordGuess(guess, answer) {
+      return guess.length > 1 || guess.length === answer.length;
+    },
+    handleFullWordGuess() {
+      if (this.guess.toLowerCase() === this.answer.toLowerCase()) {
+        console.log('Yinz won the game!');
+        this.$emit('gameWon');
+      } else {
+        console.log("I'm sorry, that was an incorrect guess!");
+        this.incorrect++;
+        if (this.incorrect === this.maxIncorrect) {
+          console.log('Game Over');
+          this.$emit('gameOver');
         }
       }
-    }
-  };
-  </script>
-  
-  <style>
-  .user-guess {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-left: 20px; 
+    },
+    handleSingleLetterGuess() {
+      const guess = this.guess.toLowerCase();
+      const answer = this.answer.toLowerCase();
+
+      console.log('Guess',guess)
+      console.log('Answer', answer)
+      console.log('Hidden', this.Hidden)
+      
+      if (!answer.includes(guess)) {
+        // Handle incorrect guess logic
+        this.incorrect++;
+        if (this.incorrect === this.maxIncorrect) {
+          console.log('Game over');
+          this.$emit('gameOver');
+        }
+      } else {
+        // Update hidden word with correctly guessed letters
+        const newHidden = this.answer.split('').map((char, index) => {
+          if (char.toLowerCase() === guess.toLowerCase()) {
+            return char;
+          } else {
+            return this.hidden[index];
+          }
+        }).join('');
+
+        this.$emit('updateHidden', newHidden);
+
+        if (!newHidden.includes('_')) {
+          console.log('Yinz win!');
+          this.$emit('gameWon');
+        }
+      }
+    },
   }
-  
-  .user-guess form {
-    margin-top: 10px;
-  }
-  
-  </style>
-  
+};
+</script>
+
+<style>
+.user-guess {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 20px;
+}
+
+.user-guess form {
+  margin-top: 10px;
+}
+</style>
